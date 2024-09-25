a6a9cd4730a675c50a56640ba0acfe277716d065
Howard Tangkulung <howardtangkulung@gmail.com>
2024-09-26 00:21:38 +0900

    added categories

--- src/Introduction.py
+++ src/Introduction.py
@@ -15,7 +15,8 @@ st.markdown("""
             [**By Howard T.**](https://howarudo.github.io/)
             """)
 
-st.markdown("""## Topics""")
-
-st.page_link("pages/1_Golden_Cross_Death_Cross.py", label="Golden Cross & Death Cross", icon="📈")
+st.markdown("""## Macro Indicators""")
 st.page_link("pages/2_The_Yield_Curve.py", label="The Yield Curve", icon="📈")
+
+st.markdown("""## Technical Indicators""")
+st.page_link("pages/1_Golden_Cross_Death_Cross.py", label="Golden Cross & Death Cross", icon="📈")
