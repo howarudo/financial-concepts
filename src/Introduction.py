73687f973e270d5da690cb027a4d1bc1cd1d89f8
Howard Tangkulung <howardtangkulung@gmail.com>
2024-10-02 01:01:09 +0900

    switched page orders

--- src/Introduction.py
+++ src/Introduction.py
@@ -8,18 +8,17 @@ st.set_page_config(
 st.sidebar.success("Select a concept from above.")
 
 st.markdown("""
-            # Financial Concepts
+            # Financial Analysis & Market Concepts
 
             A collection of market concepts, trading strategies, and investment principles visualized in Python.
 
-            [**By Howard T.**](https://howarudo.github.io/)
+            By [**Howard T.**](https://howarudo.github.io/)
             """)
 
-st.markdown("""## Market Concepts""")
-st.page_link("pages/2_The_Yield_Curve.py", label="**The Yield Curve** (*8 min read*)", icon="📈")
+st.markdown("""### Quantitative Analysis""")
+st.page_link("pages/1_The_NVIDIA_Miracle.py", label="**The NVIDIA Miracle: A Comprehensive Analysis** (*15 min read*)", icon="📈"    )
+st.page_link("pages/2_Event_Study_Analysis.py", label="**Event Study Analysis: An Introduction** (*5 min read*)", icon="📈")
 
-st.markdown("""## Market Trends and Analysis""")
-st.page_link("pages/3_Event_Study_Analysis.py", label="**Event Study Analysis** (*5 min read*)", icon="📈")
-
-st.markdown("""## Technical Indicators""")
-st.page_link("pages/1_Golden_Cross_Death_Cross.py", label="**Golden Cross & Death Cross** (*4 min read*)", icon="📈")
+st.markdown("### Other Concepts")
+st.page_link("pages/3_The_Yield_Curve.py", label="**The Yield Curve** (*8 min read*)", icon="📈")
+st.page_link("pages/4_Golden_Cross_Death_Cross.py", label="**Golden Cross & Death Cross** (*4 min read*)", icon="📈")
